e03b8e7522abb04d0e3ba1a1d2934aa19830c962
Jakub Blažík <blazikj24@spstrutnov.cz>
2026-03-03 10:28:24 +0100

    Přidání kontroleru User + User views

---
+++ VYS_WEB/VYS_WEB/Controllers/UserControllers.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using VYS_WEB.Models;
+
+namespace VYS_WEB.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register ()
+        {
+            return View();
+        }
+    }
+}
