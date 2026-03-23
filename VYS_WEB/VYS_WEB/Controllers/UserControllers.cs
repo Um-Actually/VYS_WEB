7f533228a3e659f1249a533b939ae10b795549f9
Jakub Blažík <blazikj24@spstrutnov.cz>
2026-03-23 14:18:55 +0100

    přidání přihlašování pouze přes login

--- VYS_WEB/VYS_WEB/Controllers/UserControllers.cs
+++ VYS_WEB/VYS_WEB/Controllers/UserControllers.cs
@@ -1,11 +1,20 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using VYS_WEB.Models;
+using VYS_WEB.Data;
+using System.Linq;
 
 namespace VYS_WEB.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -32,10 +41,49 @@ namespace VYS_WEB.Controllers
                 ViewData["chyba"] = "Hesla se neshodují";
                 return View();
             }
-            else
+
+            if (_db.Users.Any(u => u.Name == username))
             {
-                return Redirect("/User/Login");
+                ViewData["chyba"] = "Uživatel s tímto jménem již existuje";
+                return View();
             }
+
+            var user = new User
+            {
+                Name = username,
+                Password = password
+            };
+
+            _db.Users.Add(user);
+            _db.SaveChanges();
+
+            return Redirect("/User/Login");
+
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ViewData["chyba"] = "Jméno nebo heslo nebylo zadáno";
+                return View();
+            }
+
+            var user = _db.Users.FirstOrDefault(u => u.Name == username);
+            if (user == null)
+            {
+                ViewData["chyba"] = "Uživatel neexistuje";
+                return View();
+            }
+
+            if (user.Password != password)
+            {
+                ViewData["chyba"] = "Nesprávné heslo";
+                return View();
+            }
+
+            TempData["username"] = user.Name;
+            return Redirect("/User/Profil");
 
         }
     }
